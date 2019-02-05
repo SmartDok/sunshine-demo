e5380c5551e88c5c242c05eb8e680b9d24cb3db6
Robin Pedersen <robin@smartdok.com>
2019-02-05 07:13:29 +0100

    Update TODOs

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <li class="todo-list__item">
+  <li :class="classes">
     <slot />
   </li>
 </template>
@@ -9,6 +9,29 @@ import Vue from 'vue';
 
 export default Vue.extend({
   name: 'TodoItem',
+
+  props: {
+    done: {
+      type: Boolean,
+      default: false,
+    },
+  },
+
+  computed: {
+    classes(): object {
+      return {
+        'todo-list__item': true,
+        'todo-list__item--done': this.done,
+      };
+    },
+  },
 });
 </script>
 
+<style lang="scss" scoped>
+.todo-list__item {
+  &--done {
+    text-decoration: line-through;
+  }
+}
+</style>
--- src/views/TableDemo.vue
+++ src/views/TableDemo.vue
@@ -53,9 +53,9 @@
       <todo>Keyboard selection.</todo>
       <todo>Multi-selection checkboxes.</todo>
       <todo>Select/unselect all.</todo>
-      <todo>Prevent removing all columns.</todo>
-      <todo>Prevent moving other columns to replace the first ("row title") column. (This might be generalized to say that a configurable number of columns are fixed, or "static").</todo>
-      <todo>Optimize column operations when many rows are rendered.</todo>
+      <todo done>Prevent removing all columns.</todo>
+      <todo done>Prevent moving other columns to replace the first ("row title") column. (This might be generalized to say that a configurable number of columns are fixed, or "static").</todo>
+      <todo done>Optimize column operations when many rows are rendered.</todo>
       <todo>Smart loading when scrolling up (works only when scrolling down).</todo>
       <todo>Possibility to include sub-items synchronously.</todo>
       <todo>Automatically scroll horizontally when dragging columns.</todo>
