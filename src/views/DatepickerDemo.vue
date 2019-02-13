000c3bff2593bc7878582e512a7b175a212a38e7
Thomas Nielsen <tsn1985@gmail.com>
2019-02-13 11:45:19 +0100

    Add todo-list

--- src/views/DatepickerDemo.vue
+++ src/views/DatepickerDemo.vue
@@ -1,6 +1,14 @@
 <template>
     <div>
         <s-datepicker />
+        <todo-list>
+          <todo>Toggle datepicker with a SMenu component or an input-field</todo>
+          <todo>Move info about selected period to input-field</todo>
+          <todo>Scroll to month if selected period is outside viewable months</todo>
+          <todo>Add arrow-navigation on focus</todo>
+          <todo>Make sure text wraps correctly if datepicker width changes</todo>
+          <todo>Add possibilities to choose user-defined periods in menu</todo>
+        </todo-list>
     </div>
 </template>
 
