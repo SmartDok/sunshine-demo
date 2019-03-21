8d1acb8d71fba9f686ba11aafed2ed4ce47e34bf
Thomas Nielsen <tsn1985@gmail.com>
2019-03-21 09:36:33 +0100

    Update todolist, set dates empty

--- src/views/DatepickerDemo.vue
+++ src/views/DatepickerDemo.vue
@@ -6,33 +6,19 @@
           v-model="period"
           fromLabel="Fra dato"
           toLabel="Til dato"
-          @input="getPeriod"
         />
+
         <h2>Single date</h2>
         <s-date-single
           v-model="date"
           label="Dato"
-          @input="getDate"
         />
 
-        <s-accordion class="datepicker-props-list">
-          <s-accordion-item>
-            <template v-slot:heading>Possible props</template>
-            <li>rangeInput: boolean (adds from - to date)</li>
-            <li>selectedDate: Moment (date format (ISO 8601) 'YYYY-MM-DD' | {y, M, d}) (the default selected date)</li>
-            <li>fromDate: Moment (date format (ISO 8601) 'YYYY-MM-DD' | {y, M, d}) (the default from date)</li>
-            <li>toDate: Moment (date format (ISO 8601) 'YYYY-MM-DD' | {y, M, d}) (the default to date)</li>
-          </s-accordion-item>
-        </s-accordion>
-
         <todo-list>
-          <todo>Split s-date-field into two components for range / single</todo>
-          <todo>Wrap s-text-field in moment -> string converter</todo>
           <todo>Make v-model optional</todo>
+          <todo>Slected current month should send filter "current month" and not just dates (ie. 01.03.2019 - 31.03.2019)</todo>
           <todo>Scroll to month if selected period is outside viewable months</todo>
-          <todo>Fix inline style on "s-datepicker__grid__container" in DatepickerCalendar</todo>
           <todo>Add ability to zoom out: month-view, year-view</todo>
-          <todo>Styling of date-formatted input</todo>
           <todo>Styling of multiple inputs</todo>
           <todo>Add arrow-navigation on focus</todo>
           <todo>Make sure text wraps correctly if datepicker width changes</todo>
@@ -44,28 +30,29 @@
 
 <script lang="ts">
 import Vue from 'vue';
-import moment from 'moment';
+import moment, { Moment } from 'moment';
 
 export default Vue.extend({
   name: 'DatepickerDemo',
 
   data() {
     return {
-      date: moment('2019-03-24'),
+      date: {} as Moment,
       period: {
-        from: moment({ y: 2019, M: 2, d: 3 }),
-        to: moment('2019-03-24'),
+        from: {} as Moment,
+        to: {} as Moment,
       }
     }
   },
 
-  methods: {
-    getPeriod(payload: any) {
-      console.log('Get Period ', payload);
+  watch: {
+    date(newVal) {
+      console.log('Date ', moment(newVal).format('DD-MM-YYYY'));
     },
 
-    getDate(payload: any) {
-      console.log('Get date ', payload);
+    period(newVal) {
+      console.log('Period from', moment(newVal.from).format('DD-MM-YYYY'));
+      console.log('Period to', moment(newVal.to).format('DD-MM-YYYY'));
     },
   },
 });
