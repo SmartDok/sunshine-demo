6b6992439acd3fda7c1e3a1fca85737a11d6ebd4
Robin Pedersen <robin@smartdok.com>
2019-03-24 16:29:04 +0100

    Update text-area demo

--- src/views/TextAreaDemo.vue
+++ src/views/TextAreaDemo.vue
@@ -1,19 +1,20 @@
 <template>
   <div>
     <h2>Standard inputs</h2>
-    <example :code="standard" :data="{ text: ``, text2:``}" />
+    <p class="help">By default, text areas automatically get bigger when text is added.</p>
+    <example :code="standard" :data="{ text: '' }"/>
 
     <h2>With placeholder</h2>
-    <example :code='placeholder' :data="{ text:`` , text2: `` }" />
+    <example :code="placeholder" :data="{ text: '' }"/>
 
     <h2>Read only</h2>
-    <example :code='readonly' />
+    <example :code="readonly"/>
 
     <h2>Inactive inputs</h2>
     <example :code="inactive"/>
 
     <todo-list>
-      <todo>Fit content attribute? Given content as value, TextArea displays all the content. </todo>
+      <todo>Fit content attribute? Given content as value, TextArea displays all the content.</todo>
       <todo>State when in readonly?</todo>
       <todo>Scrollbard design?</todo>
     </todo-list>
@@ -21,38 +22,37 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
+import Vue from "vue";
 
 export default Vue.extend({
-  name: 'TextAreaDemo',
+  name: "TextAreaDemo",
 
   computed: {
     standard() {
       return `\
         <s-text-area label="Comments" v-model="text" />
-        <s-text-area label="Autoresize" v-model="text2" :initrows="4" :maxrows="8" />
-    `;
+      `;
     },
 
     placeholder() {
       return `\
-        <s-text-area placeholder="Placeholder.." v-model="text" />
-        <s-text-area placeholder="Placeholder.." label="Label" v-model="text2" />
-        `;
+        <p><s-text-area placeholder="Placeholder.." v-model="text" /></p>
+        <p><s-text-area placeholder="Placeholder.." label="Label" v-model="text" /></p>
+      `;
     },
 
     readonly() {
       return `\
         <s-text-area value="Lorum ipsum dolor sit amet." readonly />
-        `;
+      `;
     },
 
     inactive() {
       return `\
-        <s-text-area label="Inactive area" inactive/>
-        <s-text-area label="Inactive area" value="With text" inactive/>
-        `;
-    },
-  },
+        <p><s-text-area label="Inactive area" inactive/></p>
+        <p><s-text-area label="Inactive area" value="With text" inactive/></p>
+      `;
+    }
+  }
 });
 </script>
