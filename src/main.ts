b6a37d4a7b95029b23fffe254f2b120ab8dff430
Robin Pedersen <robin@smartdok.com>
2019-04-17 08:25:27 +0200

    Add l10n with locize.io

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import Vue from 'vue';
 import VueI18n from 'vue-i18n';
 import VeeValidate from 'vee-validate';
 import SmartDokUI from 'smartdok-sunshine';
+import locizer from 'locizer';
 import App from './App.vue';
 import router from './router';
 import Example from './components/Example.vue';
@@ -33,30 +34,55 @@ Vue.component('todo', TodoItem);
 Vue.component('props-list', PropsList);
 Vue.component('props-item', PropsItem);
 
-const i18n = new VueI18n({
-  locale: 'en',
-  messages: {
-    en: {
-      save: 'Save',
-      restore: 'Restore',
-    },
-
-    nb: {
-      save: 'Lagre',
-      restore: 'Gjenopprett',
-    },
-
-    sv: {
-      save: 'Spara',
-      restore: 'Gjenopprett',
-    },
-  },
+locizer.init({
+  fallbackLng: 'en', // load this if detected lng is not support
+  referenceLng: 'en', // the source language
+  projectId: '3773bbf9-2768-4712-9eba-d2bd5d73601c', // your locize project id
+  apiKey: 'f9a57b12-5a45-4daa-8beb-c3433ba4a5a3', // your locize api key
 });
 
-new Vue({
-  router,
-  store,
-  i18n,
+locizer.load('translations', (err, translations, detectedLng) => {
+  // build message catalog format
+  const messages = {
+    [detectedLng]: translations,
+  };
 
-  render: (h) => h(App),
-}).$mount('#app');
+  // Create VueI18n instance with options
+  const i18n = new VueI18n({
+    locale: detectedLng, // set locale
+    messages, // set locale messages
+    missing: (locale, path, vue) => {
+      // pipe to locize - that key will be created for you
+      locizer.add('translations', path, path);
+    },
+  });
+
+  // Create a Vue instance with `i18n` option
+  new Vue({
+    router,
+    store,
+    i18n,
+
+    render: (h) => h(App),
+  }).$mount('#app');
+});
+
+// const i18n = new VueI18n({
+//   locale: 'en',
+//   messages: {
+//     en: {
+//       save: 'Save',
+//       restore: 'Restore',
+//     },
+
+//     nb: {
+//       save: 'Lagre',
+//       restore: 'Gjenopprett',
+//     },
+
+//     sv: {
+//       save: 'Spara',
+//       restore: 'Gjenopprett',
+//     },
+//   },
+// });
